8af8a425e3a14b0885228930eb6ab9236581eefa
Loanvi <a.andreylog@gmail.com>
2018-05-27 13:49:08 +0300

    added model

---
+++ model/src/main/scala/model/package.scala
@@ -0,0 +1,20 @@
+import java.time.{LocalDate, LocalTime}
+
+package object model {
+  case class Company(id: Int, name: String)
+
+  case class Passenger(id: Int, name: String)
+
+  case class PassengerInTrip(tripId: Int,
+                             passengerId:Int,
+                             date : LocalDate,
+                             place:Int )
+
+  case class Trip(id : Int,
+                  companyId: Int,
+                  plane: String,
+                  townFrom: String,
+                  townTo: String,
+                  timeOut: LocalTime,
+                  timeIn: LocalTime)
+}
